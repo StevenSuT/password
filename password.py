03be126cf86bab3108a93b16c2e08275de64f42d
Steven Su <enmagic.rss@gmail.com>
2023-09-11 21:47:56 +0800

    update message

--- password.py
+++ password.py
@@ -3,10 +3,13 @@ password = 'a123456'
 i = 3
 
 while i > 0:
+	i = i - 1
 	pwd_user = input('請輸入密碼: ')
 	if pwd_user == password:
 		print('登入成功')
 		break
 	else:
-		i = i - 1
-		print('密碼錯誤，還有', i, '次機會')
+		if i > 0:
+			print('密碼錯誤，還有', i, '次機會')
+		else:
+			print('沒機會嘗試了')
